d8744ce1053dc9fef807a797512000f627b8706f
yemicoded <caliwebsolutions4108@gmail.com>
2022-10-23 16:42:28 +0100

    More Update

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -2,17 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { clx } from "../utils/clx";
 
-export default function Modal({ isOpen, classname, children }) {
-      const classes = clx(
-        "absolute w-full h-full bg-black/20 inset-0",
-        classname
-      );
+export default function Modal({ isOpen, setOpem, classname, children }) {
+  const classes = clx("absolute w-full h-full bg-black/20 inset-0", classname);
   return ReactDOM.createPortal(
-    isOpen && (
-      <div className={classes}>
-        {children}
-      </div>
-    ),
+    isOpen && <div className={classes}>{children}</div>,
     document.getElementById("signup-modal")
   );
 }
