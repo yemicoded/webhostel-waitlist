e54ced52c6ba5938545abda4f74974b4133f59ca
yemicoded <caliwebsolutions4108@gmail.com>
2022-10-23 16:50:27 +0100

    webhostel waitlist

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { clx } from "../utils/clx";
 
-export default function Modal({ isOpen, setOpem, classname, children }) {
+export default function Modal({ isOpen, classname, children }) {
   const classes = clx("absolute w-full h-full bg-black/20 inset-0", classname);
   return ReactDOM.createPortal(
     isOpen && <div className={classes}>{children}</div>,
